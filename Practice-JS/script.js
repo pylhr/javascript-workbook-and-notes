95fc44a4b30b5358cb91f17c8877a0e96f024ecb
priyanshu lohar <priyanshulohar1@gmail.com>
2024-06-29 15:37:16 +0530

    revamp

--- Practice-JS/script.js
+++ Practice-JS/script.js
@@ -1,5 +1,7 @@
-alert('i am from script.js outside')
-
+//alert creation
+//alert('i am from script.js outside')
+//html element creation using document method
+/*
 const div = document.getElementById('boilerplate')
 
 const header = document.createElement('h2');
@@ -9,3 +11,4 @@ const headerText = document.createTextNode('hey I\'m too from script.js' );
 header.appendChild(headerText);
 
 document.body.appendChild(header)
+*/
